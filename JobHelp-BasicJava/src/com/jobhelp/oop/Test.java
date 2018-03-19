0109770cd382471ff33a1bdb69834d75127b43fb
Asadul Haque <haq.asad000@gmail.com>
2018-03-18 21:33:33 -0400

    Added Animal Class
    Animal Class added to package: oop and Test class updated accordingly

--- JobHelp-BasicJava/src/com/jobhelp/oop/Test.java
+++ JobHelp-BasicJava/src/com/jobhelp/oop/Test.java
@@ -1,9 +1,42 @@
 package com.jobhelp.oop;
 
+//======================================================================
+//Subject:	Test Class
+//Author:	Asadul Haque
+//Date:		03/14/2018
+//======================================================================
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//******************************************************************
+		//							ANIMAL CLASS
+	    //******************************************************************
+			
+			
+			//==============================================================
+			// ANIMAL CLASS INSTANTIATION (creating object for animal class)
+			//==============================================================		
+			
+			Animal animal1 = new Animal();
+			
+			//==============================================================
+			// accessing all the animal class properties
+			//==============================================================
+			
+			System.out.println("Printing all the instance variables of animal class\n");
+			System.out.println(animal1.food);
+			System.out.println(animal1.location);
+			System.out.println(animal1.hunger);
+			System.out.println(animal1.habitat);
+			
+			System.out.println("\nCalling all the methods of animal class\n");
+			animal1.eat();
+			animal1.makeNoise();
+			animal1.sleep();
+			animal1.roam();
+		
 
 	}
 
